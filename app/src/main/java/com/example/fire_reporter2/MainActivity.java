2fc00f91e995538b717c3d03dba6eae39c6c3fc4
quynh16 <qtd.nguyen16@gmail.com>
2021-10-21 03:00:01 -0500

    got imageview in scrollbar working lol

--- app/src/main/java/com/example/fire_reporter2/MainActivity.java
+++ app/src/main/java/com/example/fire_reporter2/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, ProfileActivity.class));
             }
+
+
         });
     }
 }
