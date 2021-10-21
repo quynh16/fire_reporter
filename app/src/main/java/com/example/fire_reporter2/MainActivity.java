e8af5aa924b395a0c7fd3eebe1d87c9b476bd92f
quynh16 <qtd.nguyen16@gmail.com>
2021-10-21 01:27:07 -0500

    Initial commit

---
+++ app/src/main/java/com/example/fire_reporter2/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.fire_reporter2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton btn = (ImageButton)findViewById(R.id.profile_btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+            }
+        });
+    }
+}
