0c4fd7f5c8849ece87492b28089c4ccdc9aacac9
quynh16 <qtd.nguyen16@gmail.com>
2021-11-22 13:41:35 -0600

    implemented report cards for report history, can now generate cards from java code (no longer hard coded)

---
+++ app/src/main/java/com/example/fire_reporter2/EditProfileActivity.java
@@ -0,0 +1,58 @@
+package com.example.fire_reporter2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class EditProfileActivity extends AppCompatActivity {
+    BottomNavigationView navbar;
+    LinearLayout report_history;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+        navbar = findViewById(R.id.bottom_navbar);
+
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("users");
+
+        UserHelperClass helper = new UserHelperClass();
+        // get all values from text fields
+
+        navbar.setSelectedItemId(R.id.home);
+        navbar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.reporting:
+                        startActivity(new Intent(getApplicationContext(), ReportingActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.map:
+                        startActivity(new Intent(getApplicationContext(), MapActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
