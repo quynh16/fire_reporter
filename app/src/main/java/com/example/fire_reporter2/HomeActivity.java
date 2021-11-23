980bacf7eb9c4de14fb174973ba7854af2436ff2
Felipe <felipeespinoza403014@gmail.com>
2021-11-22 18:57:53 -0600

    Homepage Changes: Added Headers for Report Status and News Feed. Place holders for both Report Status and News Feed were added. Facts Slider was added.

--- app/src/main/java/com/example/fire_reporter2/HomeActivity.java
+++ app/src/main/java/com/example/fire_reporter2/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.fire_reporter2;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,12 +13,48 @@ import android.widget.ImageButton;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.util.ArrayList;
+
 public class HomeActivity extends AppCompatActivity {
+    ViewPager2 viewPager2;
+    ArrayList<ViewPagerItem> viewPagerItemArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        viewPager2 = findViewById(R.id.factsView);
+        String heading = "Fire Fact";
+        String[] desc = {getString(R.string.a_desc),
+                getString(R.string.b_desc),
+                getString(R.string.c_desc),
+                getString(R.string.d_desc)
+                ,getString(R.string.e_desc)};
+
+        viewPagerItemArrayList = new ArrayList<>();
+
+        for (int i =0; i< 5 ; i++){
+
+            ViewPagerItem viewPagerItem = new ViewPagerItem(heading,desc[i]);
+            viewPagerItemArrayList.add(viewPagerItem);
+            if(i==5){
+                i=0;
+            }
+
+        }
+
+        VPAdapter vpAdapter = new VPAdapter(viewPagerItemArrayList);
+
+        viewPager2.setAdapter(vpAdapter);
+
+        viewPager2.setClipToPadding(false);
+
+        viewPager2.setClipChildren(false);
+
+        viewPager2.setOffscreenPageLimit(2);
+
+        viewPager2.getChildAt(0).setOverScrollMode(View.OVER_SCROLL_NEVER);
 
         ImageButton btn = (ImageButton)findViewById(R.id.profile_btn);
 
